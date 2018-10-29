0548cc3c0f26845c0b5d6d70a6c6eec0e22cf211
s2ine <ba6uckE@gmail.com>
2018-10-29 21:52:12 +0600

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap bkgnd("C:/background.jpg");
+        bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+        QPalette palette;
+        palette.setBrush(QPalette::Background, bkgnd);
+        this->setPalette(palette);
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+
+
+
+    delete ui;
+}
